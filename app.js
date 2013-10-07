e599c96c6d0343bc97dc03bfb126d56724c44521
Jeremy <jermy.greaux@gmail.com>
2013-10-07 22:39:13 +0200

    first commit

--- app.js
+++ app.js
@@ -27,7 +27,31 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+ 
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+ 
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hé ho, c\'est privé ici !');
+});
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+app.get('/etage/:etagenum/chambre/:chambrenum', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à la chambre '+ req.params.chambrenum+' de l\'étage n°' + req.params.etagenum);
+});
 
-http.createServer(app).listen(process.env.PORT, process.env.IP);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
